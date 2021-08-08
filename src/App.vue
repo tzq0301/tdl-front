082925561bef4dbb539fc5334a86993213352d04
tzq0301 <1661610563@qq.com>
2021-08-08 21:00:31 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  TZQ&nbsp;&&nbsp;DJ
+  <el-button type="primary">主要按钮</el-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+
   }
 })
 </script>
